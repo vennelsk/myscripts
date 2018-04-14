a56de3d3c1babd4660615ee3cba4399b6cae59d8
Vipin <nixvipin@gmail.com>
2018-04-13 23:43:26 -0400

    initial commit

--- build_code_auto.sh
+++ build_code_auto.sh
@@ -3,19 +3,19 @@
 cd /data/myproj/myproject/employees
 mvn clean install
 
-cd /data/apache-tomcat-7.0.85/bin
+cd /data/apache-tomat/bin
 ./shutdown.sh
 sleep 10
-cd /data/apache-tomcat-7.0.85/webapps
+cd /data/apache-tomcat/webapps
 mkdir -p /data/backup/
-zip -r /data/backup/employee_`date +%d_%h_%H_%M_%S`.zip
-rm -rf /data/apache-tomcat-7.0.85/webapps/employee/*
-mkdir -p /data/apache-tomcat-7.0.85/webapps/employee/
-cp -a /data/myproj/myproject/employees/target/SpringHibernateExample.war /data/apache-tomcat-7.0.85/webapps/employee/
-cd /data/apache-tomcat-7.0.85/webapps/employee/
+zip -r /data/backup/employee_`date +%d_%h_%H_%M_%S`.zip /data/apache-tomcat/webapps/employee
+rm -rf /data/apache-tomcat/webapps/employee/*
+mkdir -p /data/apache-tomcat/webapps/employee/
+cp -a /data/myproj/myproject/employees/target/SpringHibernateExample.war /data/apache-tomcat/webapps/employee/
+cd /data/apache-tomcat/webapps/employee/
 jar -xf SpringHibernateExample.war
 rm SpringHibernateExample.war
-cd /data/apache-tomcat-7.0.85/bin
+cd /data/apache-tomcat/bin
 ./startup.sh
 sleep 5
 #/data/myproj/myproject2/HotelReservation/target/HotelReservation-0.0.1-SNAPSHOT.war
